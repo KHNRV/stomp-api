a8753c3d70c6d714dc1c390d88823bc65c84a19a
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-12 12:58:09 -0400

    Implement event relationship for judges

--- db/schema/03_judges.sql
+++ db/schema/03_judges.sql
@@ -3,5 +3,6 @@ CREATE TABLE "judges" (
   "first_name" varchar(255) NOT NULL,
   "last_name" varchar(255) NOT NULL,
   "email" varchar(255),
-  "phone" varchar(255)
+  "phone" varchar(255),
+  "event_id" integer
 );
