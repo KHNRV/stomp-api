1de6e8ad181dab94de48202dd14513c4995fcb1f
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-12 17:44:25 -0400

    Implement basic db api test

--- routes/events.js
+++ routes/events.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-module.exports = (dbHelpers) => {
-    /* GET users listing. */
-    router.get('/', (req, res) => {
-        dbHelpers.getEvents()
-            .then((users) => res.json(users))
-            .catch((err) => res.json({
-                error: err.message
-            }));
-    });
+module.exports = (db) => {
+  /* GET users listing. */
+  router.get("/", (req, res) => {
+    db.getEvents()
+      .then((users) => res.json(users))
+      .catch((err) =>
+        res.json({
+          error: err.message,
+        })
+      );
+  });
 
-    return router;
+  return router;
 };
