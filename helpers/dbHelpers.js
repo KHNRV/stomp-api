65c1849e9a2634b16f63590c499da59cc294545c
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-12 21:05:57 -0400

    Implement scores and competitions readers

--- helpers/dbHelpers.js
+++ helpers/dbHelpers.js
@@ -1,67 +1,86 @@
 module.exports = (db) => {
-  const getUserByEmail = (email) => {
-    const query = {
-      text: `SELECT * FROM users WHERE email = $1`,
-      values: [email],
-    };
-
-    return db
-      .query(query)
-      .then((result) => result.rows[0])
-      .catch((err) => err);
-  };
-
-  const addUser = (firstName, lastName, email, password) => {
-    const query = {
-      text: `INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *`,
-      values: [firstName, lastName, email, password],
-    };
-
-    return db
-      .query(query)
-      .then((result) => result.rows[0])
-      .catch((err) => err);
-  };
-
-  const getUsersPosts = () => {
-    const query = {
-      text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
-      FROM users
-      INNER JOIN posts
-      ON users.id = posts.user_id`,
-    };
-
-    return db
-      .query(query)
-      .then((result) => result.rows)
-      .catch((err) => err);
-  };
-
   return {
     events: {
-      all() {
-        const query = {
-          text: ` SELECT id, event_code, event_name, email FROM events`,
-        };
+      read: {
+        all() {
+          const query = {
+            text: ` SELECT id, event_code, event_name, email FROM events`,
+          };
 
-        return db
-          .query(query)
-          .then((result) => result.rows)
-          .catch((err) => err);
+          return db
+            .query(query)
+            .then((result) => result.rows)
+            .catch((err) => err);
+        },
+
+        findByEventCode(event_code) {
+          const query = {
+            text: ` SELECT id, event_code, event_name, email  
+                    FROM events
+                    WHERE event_code = $1`,
+            values: [event_code],
+          };
+
+          return db
+            .query(query)
+            .then((result) => result.rows[0])
+            .catch((err) => err);
+        },
       },
+    },
+    competitions: {
+      read: {
+        filterByEventCode(event_code) {
+          const query = {
+            text: ` WITH event_id AS (
+                            SELECT id
+                            FROM events
+                            WHERE event_code = $1
+                    )
+                    SELECT  competitions.id, 
+                            competitions.name, 
+                            competitions.scoring_system_id, 
+                            array_agg(DISTINCT entries.participant_id) AS participants,
+                            array_agg(DISTINCT judges_competitions.judge_id) AS judges,
+                            competitions.head_judge_id
+                    FROM competitions, entries, judges_competitions
+                    WHERE competitions.event_id = event_id
+                    AND entries.competition_id = competitions.id
+                    AND judges_competitions.competition_id = competitions.id
+                    GROUP BY competitions.id;`,
+            values: [event_code],
+          };
 
-      getByCode(event_code) {
-        const query = {
-          text: ` SELECT id, event_code, event_name, email  
-                  FROM events
-                  WHERE event_code = $1`,
-          values: [event_code],
-        };
+          return db
+            .query(query)
+            .then((result) => result.rows)
+            .catch((err) => err);
+        },
+      },
+    },
+    scores: {
+      read: {
+        filterByCompetitionId(competition_id) {
+          const query = {
+            text: ` SELECT DISTINCT entries.participant_id AS participant_id,
+                                judges_competitions.judge_id AS judge_id,
+                                scores.value AS score
+                    FROM competitions
+                    JOIN entries ON entries.competition_id = competitions.id
+                    JOIN judges_competitions ON judges_competitions.competition_id = competitions.id
+                    LEFT JOIN scores ON entries.id = scores.entry_id
+                    WHERE competitions.id = $1
+                    AND entries.competition_id = competitions.id
+                    AND judges_competitions.competition_id = competitions.id
+                    ORDER BY participant_id, judge_id`,
+            values: [competition_id],
+          };
 
-        return db
-          .query(query)
-          .then((result) => result.rows[0])
-          .catch((err) => err);
+          return db
+            .query(query)
+            .then((result) => result.rows)
+            .catch((err) => err);
+        },
       },
     },
   };
