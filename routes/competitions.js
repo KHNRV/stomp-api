1afff4bef80264067ff76ce4a45687a83069209b
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-12 21:51:44 -0400

    Create competition route

--- routes/competitions.js
+++ routes/competitions.js
@@ -2,10 +2,10 @@ const competitions = require("express").Router({ mergeParams: true });
 
 module.exports = (db) => {
   competitions.get("/", (req, res) => {
-    console.log(req.params.event_code);
     const event_code = req.params.event_code;
-    db.competitions.read
-      .filterByEventCode(event_code)
+    db.events.read.id
+      .findByEventCode(event_code)
+      .then((event_id) => db.competitions.read.filterByEventId(event_id))
       .then(async(competitions) => {
         for (const competition of competitions) {
           competition.scores = await db.scores.read.filterByCompetitionId(
