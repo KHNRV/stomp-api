95d9879d1875384327950d457fb910dade7a54b9
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-13 21:38:48 -0400

    Implemant create route for competitions

--- routes/competitions.js
+++ routes/competitions.js
@@ -14,11 +14,33 @@ module.exports = (db) => {
         }
         res.json(competitions);
       })
-      .catch((err) =>
-        res.json({
-          error: err.message,
-        })
-      );
+      .catch((err) => res.status(400).json({ status: "error", message: err }));
   });
+
+  competitions.post("/", (req, res) => {
+    const event_code = req.params.event_code;
+    const competition = req.body;
+    let event_id;
+
+    db.events.read.id
+      .findByEventCode(event_code)
+      .then((id) => {
+        event_id = id;
+        return db.competitions.create(event_id, competition);
+      })
+      .then(() => db.competitions.read.filterByEventId(event_id))
+      .then(async(competitions) => {
+        for (const competition of competitions) {
+          competition.scores = await db.scores.read.filterByCompetitionId(
+            competition.id
+          );
+        }
+        res.json(competitions);
+      })
+      .catch((err) => res.status(400).json({ status: "error", message: err }));
+  });
+  competitions.put("/:competition_id", (req, res) => {});
+  competitions.delete("/:competition_id", (req, res) => {});
+
   return competitions;
 };
