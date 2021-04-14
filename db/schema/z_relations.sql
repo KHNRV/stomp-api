ff236f208609849f91f5164a71ef32bc35a53c4c
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-13 21:37:39 -0400

    Add a CASCADE uppon competition removal

--- db/schema/z_relations.sql
+++ db/schema/z_relations.sql
@@ -8,9 +8,9 @@ ALTER TABLE "participants" ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("participant_id") REFERENCES "participants" ("id");
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("competition_id") REFERENCES "competitions" ("id");
+ALTER TABLE "entries" ADD FOREIGN KEY ("competition_id") REFERENCES "competitions" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "judges_competitions" ADD FOREIGN KEY ("competition_id") REFERENCES "competitions" ("id");
+ALTER TABLE "judges_competitions" ADD FOREIGN KEY ("competition_id") REFERENCES "competitions" ("id") ON DELETE CASCADE;
 
 ALTER TABLE "judges_competitions" ADD FOREIGN KEY ("judge_id") REFERENCES "judges" ("id");
 
