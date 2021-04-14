0d554b8667887ca963108ea2d6aa2de33a398263
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-13 21:37:08 -0400

    Fix bug where evend_id could be NULL

--- db/schema/05_competitions.sql
+++ db/schema/05_competitions.sql
@@ -2,6 +2,6 @@ CREATE TABLE "competitions" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "name" varchar(255) NOT NULL,
   "scoring_system_id" integer,
-  "event_id" integer,
+  "event_id" integer NOT NULL,
   "head_judge_id" integer
 );
