3868aa6660958ab151342923828b5bf385cf05fb
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-19 17:06:38 -0400

    Fix buf for find competition by id query

--- db/queries/competitions.read.findById.sql
+++ db/queries/competitions.read.findById.sql
@@ -1,10 +1,11 @@
 SELECT  competitions.id, 
         competitions.name, 
         competitions.scoring_system_id, 
-        array_agg(DISTINCT entries.participant_id) AS participants,
-        array_agg(DISTINCT judges_competitions.judge_id) AS judges,
-        competitions.head_judge_id,
-        competitions.event_id
-FROM competitions, entries, judges_competitions
-WHERE competitions.id = $1
-GROUP BY competitions.id
+        array_remove(array_agg(DISTINCT entries.participant_id), NULL) AS participants,
+        array_remove(array_agg(DISTINCT judges_competitions.judge_id), NULL) AS judges,
+        competitions.head_judge_id
+FROM competitions
+FULL OUTER JOIN entries ON (competitions.id = entries.competition_id)
+FULL OUTER JOIN judges_competitions ON (competitions.id = judges_competitions.competition_id)
+WHERE competitions.event_id = $1
+GROUP BY competitions.id;
